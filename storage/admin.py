58d18cb6524e9f260e640991c0db0f4fc5b25363
RamandeepKaurKainth <rramandeepkaur11@my.bcit.ca>
2026-05-04 11:56:46 -0700

    add storage migration

--- storage/admin.py
+++ storage/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import StorageLocation
 
-admin.site.register(StorageLocation)
+
+@admin.register(StorageLocation)
+class StorageLocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "municipality",
+        "region",
+        "available_space",
+        "is_active",
+    )
+
+    search_fields = ("name", "municipality", "region")
