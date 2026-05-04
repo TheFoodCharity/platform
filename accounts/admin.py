2433399ec535ac75e069f6393bdd060bef638a20
Alex Krantz <alex@krantz.dev>
2026-05-04 12:04:56 -0700

    Add proxy model for group for admin ui

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group as AuthGroup
 
-from .models import User
+from .models import Group, User
+
+admin.site.unregister(AuthGroup)
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     model = User
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin):
+    pass
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group as AuthGroup
 
 
 class User(AbstractUser):
     pass
+
+
+class Group(AuthGroup):
+    class Meta:
+        proxy = True
