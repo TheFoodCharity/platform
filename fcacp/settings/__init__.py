6a66f081397b9db0546d100713be67637e039b93
Alex Krantz <alex@krantz.dev>
2026-05-04 12:17:07 -0700

    Install django organizations
    # Conflicts:
    #	pyproject.toml
    #	uv.lock

--- fcacp/settings/__init__.py
+++ fcacp/settings/__init__.py
@@ -33,7 +33,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 1st-party
     "accounts",
+    # 3rd-party
+    "organizations",
+    # Django
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
