3476afadccbb2142dbbc30799bbc9a1413b528db
RamandeepKaurKainth <rramandeepkaur11@my.bcit.ca>
2026-05-04 11:44:36 -0700

    fix lint issues

--- storage/admin.py
+++ storage/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import StorageLocation
 
 admin.site.register(StorageLocation)
--- storage/models.py
+++ storage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class StorageLocation(models.Model):
     name = models.CharField(max_length=255)
 
