edee2ac06e96bd43dd64b9637cd718f9392e99f0
Erryl <errylgalang@gmail.com>
2026-05-04 21:34:47 -0700

    Redo missing installation steps from documentation

--- fcacp/settings/__init__.py
+++ fcacp/settings/__init__.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_browser_reload",
     "tailwind",
     "theme",
 ]
@@ -53,10 +54,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
-
-if DEBUG:
-    MIDDLEWARE.append("django_browser_reload.middleware.BrowserReloadMiddleware")
 
 ROOT_URLCONF = "fcacp.urls"
 
