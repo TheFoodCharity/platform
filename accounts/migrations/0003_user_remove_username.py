6b8376fa34a1822f19097ae7a8d5a58cf77ef281
Alex Krantz <alex@krantz.dev>
2026-05-05 12:25:31 -0700

    Straighten out organization migrations

--- accounts/migrations/0003_user_remove_username.py
+++ accounts/migrations/0003_user_remove_username.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("accounts", "0005_organization_contact"),
+        ("accounts", "0002_group"),
     ]
 
     operations = [
