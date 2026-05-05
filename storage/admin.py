09716c578cce973083539a87d64421ca9b4ec75d
RamandeepKaurKainth <rramandeepkaur11@my.bcit.ca>
2026-05-05 13:30:59 -0700

    fix formatting issues

--- storage/admin.py
+++ storage/admin.py
@@ -45,68 +45,82 @@ class StorageLocationAdmin(admin.ModelAdmin):
     )
 
     fieldsets = (
-        ("Basic Information", {
-            "fields": ("name", "owner_operator", "is_active")
-        }),
-        ("Location", {
-            "fields": (
-                "address",
-                "approximate_location",
-                "municipality",
-                "region",
-                "province",
-                "postal_code",
-                "latitude",
-                "longitude",
-            )
-        }),
-        ("Contact", {
-            "fields": ("contact_person", "contact_method")
-        }),
-        ("Storage Capacity", {
-            "fields": (
-                "storage_type",
-                "accepted_food_types",
-                "dry_capacity",
-                "refrigerated_capacity",
-                "frozen_capacity",
-                "available_space",
-                "capacity_status",
-            )
-        }),
-        ("Access and Notes", {
-            "fields": (
-                "access_hours",
-                "restrictions",
-                "special_notes",
-                "admin_notes",
-            )
-        }),
-        ("Equipment and Facility Capabilities", {
-            "fields": (
-                "has_loading_dock",
-                "has_ramp_access",
-                "has_liftgate_access",
-                "has_forklift",
-                "has_pallet_jack",
-                "has_floor_jack",
-                "has_hand_truck",
-                "max_pallet_capacity",
-                "max_load_size",
-            )
-        }),
-        ("Permissions and Approval", {
-            "fields": (
-                "permission_level",
-                "approval_status",
-            )
-        }),
-        ("Verification", {
-            "fields": (
-                "last_verified_at",
-                "verification_requested_at",
-                "created_at",
-                "updated_at",
-            )
-        }),
+        ("Basic Information", {"fields": ("name", "owner_operator", "is_active")}),
+        (
+            "Location",
+            {
+                "fields": (
+                    "address",
+                    "approximate_location",
+                    "municipality",
+                    "region",
+                    "province",
+                    "postal_code",
+                    "latitude",
+                    "longitude",
+                )
+            },
+        ),
+        ("Contact", {"fields": ("contact_person", "contact_method")}),
+        (
+            "Storage Capacity",
+            {
+                "fields": (
+                    "storage_type",
+                    "accepted_food_types",
+                    "dry_capacity",
+                    "refrigerated_capacity",
+                    "frozen_capacity",
+                    "available_space",
+                    "capacity_status",
+                )
+            },
+        ),
+        (
+            "Access and Notes",
+            {
+                "fields": (
+                    "access_hours",
+                    "restrictions",
+                    "special_notes",
+                    "admin_notes",
+                )
+            },
+        ),
+        (
+            "Equipment and Facility Capabilities",
+            {
+                "fields": (
+                    "has_loading_dock",
+                    "has_ramp_access",
+                    "has_liftgate_access",
+                    "has_forklift",
+                    "has_pallet_jack",
+                    "has_floor_jack",
+                    "has_hand_truck",
+                    "max_pallet_capacity",
+                    "max_load_size",
+                )
+            },
+        ),
+        (
+            "Permissions and Approval",
+            {
+                "fields": (
+                    "permission_level",
+                    "approval_status",
+                )
+            },
+        ),
+        (
+            "Verification",
+            {
+                "fields": (
+                    "last_verified_at",
+                    "verification_requested_at",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
     )
