9560b1703163f828e5b2a9b5493951d3a6dd6f12
jhx88 <jiahongxu1997@gmail.com>
2026-05-07 22:10:00 -0700

    fix format

--- public/views.py
+++ public/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import HttpResponse, render
 def home(request):
     return render(request, "public/landing.html")
 
+
 def demo_interest(request):
     return HttpResponse("Thanks! Demo requests are open - contact us to schedule one.")
