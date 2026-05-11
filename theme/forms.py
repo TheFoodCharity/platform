02d8463b8a7363875ed41f1208b0c83de71181e5
Alex Krantz <alex@krantz.dev>
2026-05-11 12:11:53 -0700

    Allow customizing layout via class var

--- theme/forms.py
+++ theme/forms.py
@@ -1,4 +1,7 @@
+from typing import ClassVar
+
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
 
 
 class ThemedFormHelper(FormHelper):
@@ -9,6 +12,10 @@ class ThemedFormMixin:
     """Attach a ThemedFormHelper so {% crispy form %} renders fields only (no <form> tag).
     Forms that need a custom Layout() should assign self.helper.layout in their __init__."""
 
+    layout: ClassVar[Layout | None] = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = ThemedFormHelper(self)
+        if self.layout is not None:
+            self.helper.layout = self.layout
