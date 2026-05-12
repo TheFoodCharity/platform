4ef26ee541630ec0169f7f3130d3b985f1978e20
RamandeepKaurKainth <rramandeepkaur11@my.bcit.ca>
2026-05-12 01:59:44 -0700

    Connect dashboard to food help map

--- public/urls.py
+++ public/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("food-help-map/", views.food_help_map, name="food_help_map"),
+    path("food-help/", views.food_help_map, name="food_help_map"),
     path("demo-interest/", views.demo_interest, name="demo_interest"),
 ]
