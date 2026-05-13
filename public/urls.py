78ae294af692a05846affe009140952716b0af3e
RamandeepKaurKainth <rramandeepkaur11@my.bcit.ca>
2026-05-13 02:03:27 -0700

    Add operational dashboard interface

--- public/urls.py
+++ public/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("demo-interest/", views.demo_interest, name="demo_interest"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
