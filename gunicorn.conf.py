c70eca7578eb94fb1dd01914b5cc8e1b63bd6a80
Alex Krantz <alex@krantz.dev>
2026-05-19 12:22:48 -0700

    Respect web concurrency environment variable

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,7 +5,7 @@ host = environ.get("GUNICORN_HOST", "0.0.0.0")
 port = environ.get("GUNICORN_PORT", environ.get("PORT", "8000"))
 
 bind = f"{host}:{port}"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = int(environ.get("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2 + 1))
 
 loglevel = environ.get("GUNICORN_LOG_LEVEL", "info")
 accesslog = "-"
