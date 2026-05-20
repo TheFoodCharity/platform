98d2aa22a6b0991513e7704c28011dd842816c0b
Alex Krantz <alex@krantz.dev>
2026-05-19 23:20:56 -0700

    Limit celery worker concurrency

--- fcacp/celery.py
+++ fcacp/celery.py
@@ -6,4 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fcacp.settings.development")
 
 app = Celery("fcacp")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+if concurrency := os.environ.get("WORKER_CONCURRENCY"):
+    app.conf.worker_concurrency = int(concurrency)
+
 app.autodiscover_tasks()
